803a38a6b44dfaa2453ba3ae498a9ca1700ac51a
Robertloz <42167605+Dakse@users.noreply.github.com>
2019-08-22 20:01:52 +0200

    22.08 20:01

---
+++ precache-manifest.da38d24e2a0e302d1a54de0618c31fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0a1a9cf73371cc8b46d5445ad50701",
+    "url": "/onlywar/index.html"
+  },
+  {
+    "revision": "050964edc3a41b88f464",
+    "url": "/onlywar/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dce27e03eb5a087a6a76",
+    "url": "/onlywar/static/js/2.f43c4039.chunk.js"
+  },
+  {
+    "revision": "050964edc3a41b88f464",
+    "url": "/onlywar/static/js/main.38ec0731.chunk.js"
+  },
+  {
+    "revision": "d1da49313665d143f817",
+    "url": "/onlywar/static/js/runtime~main.875c518c.js"
+  }
+]);
